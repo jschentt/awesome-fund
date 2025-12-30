ac922abba95a6188ed2e47759b5ad5fffb3ac23f
chenjinsheng <chenjinsheng@yzw.cn>
2025-12-30 16:08:17 +0800

    feat: 修改部署脚本文件

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-pnpm build && scp -r .next/standalone public package.json ecosystem.config.js pnpm-lock.yaml next.config.mjs boot.sh .env root@47.120.30.113:/www/awesome-fund
+pnpm build && scp -r public package.json ecosystem.config.js pnpm-lock.yaml next.config.mjs boot.sh .env root@47.120.30.113:/www/awesome-fund
