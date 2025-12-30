6f33878a8f83e64856d08cd635afd6400f0a808e
chenjinsheng <chenjinsheng@yzw.cn>
2025-12-30 16:11:00 +0800

    feat: 优化部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-pnpm build && scp -r public package.json ecosystem.config.js pnpm-lock.yaml next.config.mjs boot.sh .env root@47.120.30.113:/www/awesome-fund
+scp -r public package.json ecosystem.config.js pnpm-lock.yaml next.config.mjs boot.sh .env root@47.120.30.113:/www/awesome-fund
