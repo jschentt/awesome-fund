be5ee4e6b8ccfda5f86ddf01f32f237784d92346
chenjinsheng <chenjinsheng@yzw.cn>
2025-12-30 17:26:54 +0800

    feat: 修改部署配置

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
 import path from 'path';
 
-// 让 Vercel 走 /，自己服务器走 /fund
-const isVercel = process.env.VERCEL === '1'; // Vercel 会自动注入 VERCEL=1
-const isLocalDev = process.env.NODE_ENV === 'development'; // 本地开发环境
-const basePath = isVercel || isLocalDev ? '' : '/fund';
+const isVercel = process.env.VERCEL === '1'; // Vercel 会自动注
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: isVercel ? undefined : 'standalone', // Vercel 不需要 standalone
-    basePath,
     images: {
         remotePatterns: [
             {
