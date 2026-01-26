e06798af10c4a54530cf0fd72c21307014b241b7
chenjinsheng <chenjinsheng@yzw.cn>
2026-01-26 16:06:03 +0800

    feat: 构建命令行优化

--- deploy.sh
+++ deploy.sh
@@ -178,7 +178,7 @@ show_deployment_info() {
     echo "  ssh -p $PORT $SERVER_USER@$SERVER_HOST 'tail -f /var/log/awesome-fund.log'"
     echo ""
     echo "访问应用:"
-    echo "  https://maiqishare.xyz"
+    echo "  https://sleeve.maiqishare.xyz"
     echo ""
 }
 
